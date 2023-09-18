8a50d684c22f67a305b45dc9a137b7a2dccdd3a0
AcrylicShrimp <led789zxpp@naver.com>
2023-09-18 19:54:01 +0900

    feat: add needed derive impls on event types.

--- src/event/event_types/mod.rs
+++ src/event/event_types/mod.rs
@@ -1,2 +1,5 @@
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Update;
+
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct LateUpdate;
